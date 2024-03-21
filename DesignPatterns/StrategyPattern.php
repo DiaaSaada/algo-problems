ea370e83bf740c77c7339688fddc62f2ccb01654
Diaa <dia.saada@gmail.com>
2024-03-21 13:58:01 +0400

    Update StrategyPattern.php

--- DesignPatterns/StrategyPattern.php
+++ DesignPatterns/StrategyPattern.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ *Strategy is a behavioral design pattern that
+ * lets you define a family of algorithms,
+ * put each of them into a separate class,
+ * and make their objects interchangeable.
+ */
 namespace StrategyPattern;
 interface IEngine
 {
