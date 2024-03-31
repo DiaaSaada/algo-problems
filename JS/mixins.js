dcd682eaac1edccfcda8106fb1574f62e0bc5bcc
Diaa <dia.saada@gmail.com>
2024-03-31 21:17:42 +0400

    Using mixing in JS

---
+++ JS/mixins.js
@@ -0,0 +1,15 @@
+class Car {
+    test() {
+        console.log("rev");
+    }
+}
+
+const mixin1 = {
+    rev() {
+        console.log("rev");
+    }
+}
+
+// adding the mixin "mixin1" to Class Car to extend it
+Object.assign(Car.prototype, mixin1)
+const car = new Car()
