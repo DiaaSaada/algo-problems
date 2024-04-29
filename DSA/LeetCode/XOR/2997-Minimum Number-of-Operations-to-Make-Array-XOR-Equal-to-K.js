f861732d34c2dc541754a4d608572c7916e087cd
Diaa AbuSaada <dia.saada@gmail.com>
2024-04-29 11:31:16 +0400

    Update 2997-Minimum Number-of-Operations-to-Make-Array-XOR-Equal-to-K.js

--- DSA/LeetCode/XOR/2997-Minimum Number-of-Operations-to-Make-Array-XOR-Equal-to-K.js
+++ DSA/LeetCode/XOR/2997-Minimum Number-of-Operations-to-Make-Array-XOR-Equal-to-K.js
@@ -5,20 +5,20 @@
  */
 var minOperations = function (nums, k) {
 
-    let res = nums[0]
+    let bitwiseNums = nums[0]
     for (i = 1; i < nums.length; i++) {
-        res = res ^ nums[i]
+        bitwiseNums = bitwiseNums ^ nums[i]
 
     }
-    console.log(res);
-    if (res == k)
+    
+    if (bitwiseNums == k)
         return 0
 
-    let diff = res ^ k;
-    console.log(diff);
+    let diff = bitwiseNums ^ k;
+    
     diff = (diff >>> 0).toString(2).split("")
     let sum = 0;
-    console.log((7 >>> 0).toString(2))
+    
 
     for (a of diff) {
         sum += parseInt(a)
