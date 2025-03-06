a786b30ba1326aa24229bcbbafe819478348f52b
Diaa <dia.saada@gmail.com>
2025-03-06 08:50:02 +0400

    DSA\LeetCode\Arrays\2965. Find Missing and Repeated Values.py (revised)

--- DSA/LeetCode/Arrays/2965. Find Missing and Repeated Values.py
+++ DSA/LeetCode/Arrays/2965. Find Missing and Repeated Values.py
@@ -4,18 +4,17 @@ class Solution:
         n = len(grid)
         map_ = {}
         a = None
-        
+        sum_all = (n*n)*((n*n) +1)//2  # sum all elements from 1 to n
         for r in range(n):
             for c in range(n):
+                
                 if grid[r][c] in map_:
                     a = grid[r][c]
+                else:
+                    sum_all -= grid[r][c]
                 map_[grid[r][c]] =  True
 
-        b = None
-        for i in range(1, n*n+1):
-            if i not in map_:
-                b = i
-                return [a,b]
+ 
 
-        return [a,b]
+        return [a,sum_all]
         
